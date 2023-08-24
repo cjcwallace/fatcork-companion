9f26b7e4e99a9b22b2dabc143316abe0ccd8ffd1
cameronCJW <cmrnwllc@gmail.com>
2023-08-23 22:00:34 -0700

    fix reducer not saving first and last name to state

--- store/global/reducer.js
+++ store/global/reducer.js
@@ -1,13 +1,18 @@
 import * as SecureStore from 'expo-secure-store';
 
-import { authActionTypes, cuveeActionTypes, vendorActionTypes } from './actions';
+import {
+  authActionTypes,
+  cuveeActionTypes,
+  inventoryActionTypes,
+  vendorActionTypes,
+} from './actions';
 
 import { SecureStoreEnum } from '../../utils/SecureStore';
 
 console.log('in here');
 export const authReducer = (state, action) => {
-  console.log(`state = ${JSON.stringify(state)}`);
-  console.log(`action = ${JSON.stringify(action)}`);
+  console.log(`auth start state = ${JSON.stringify(state)}`);
+  console.log(`auth start action = ${JSON.stringify(action)}`);
   switch (action.type) {
     case authActionTypes.RESTORE_TOKEN:
       return {
@@ -18,14 +23,14 @@ export const authReducer = (state, action) => {
     case authActionTypes.LOGIN:
       console.log(`login payload ${JSON.stringify(action.payload)}`);
       SecureStore.setItemAsync(SecureStoreEnum.TOKEN, action.payload.token);
+      // SecureStore.setItemAsync(SecureStoreEnum.USER, action.payload.user.id);
       return {
         ...state,
         isAuthenticated: true,
-        // user: action.payload.user,
         user: {
           id: action.payload.id,
-          firstName: action.payload.firstName,
-          lastName: action.payload.lastName,
+          firstName: action.payload.first_name,
+          lastName: action.payload.last_name,
           email: action.payload.email,
         },
         token: action.payload.token,
@@ -40,17 +45,46 @@ export const authReducer = (state, action) => {
       };
     case authActionTypes.REGISTER:
       SecureStore.setItemAsync(SecureStoreEnum.TOKEN, action.payload.token);
-      // SecureStore.setItemAsync(SecureStoreEnum.USER, initialState.user);
+      // SecureStore.setItemAsync(SecureStoreEnum.USER, action.payload.user.id);
+      console.log(`register state = ${JSON.stringify(state)}`);
+      console.log(`register action = ${JSON.stringify(action)}`);
       return {
         ...state,
         isAuthenticated: true,
         user: {
           id: action.payload.id,
-          firstName: action.payload.firstName,
-          lastName: action.payload.lastName,
+          firstName: action.payload.first_name,
+          lastName: action.payload.last_name,
           email: action.payload.email,
         },
         token: action.payload.token,
+      };
+    default:
+      return state;
+  }
+};
+
+export const inventoryReducer = (state, action) => {
+  console.log(` type = ${action.type}`);
+  switch (action.type) {
+    case inventoryActionTypes.REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+        hasError: false,
+      };
+    case inventoryActionTypes.SUCCESS:
+      console.log('inventory success');
+      return {
+        ...state,
+        isFetching: false,
+        cuvees: action.payload,
+      };
+    case inventoryActionTypes.FAILURE:
+      return {
+        ...state,
+        hasError: true,
+        isFetching: false,
       };
     default:
       return state;
@@ -61,19 +95,21 @@ export const cuveeReducer = (state, action) => {
   console.log(` type = ${action.type}`);
   switch (action.type) {
     case cuveeActionTypes.REQUEST:
+      console.log('cuvee request');
       return {
         ...state,
         isFetching: true,
         hasError: false,
       };
     case cuveeActionTypes.SUCCESS:
-      console.log('success');
+      console.log('cuvee success');
       return {
         ...state,
         isFetching: false,
-        cuvees: action.payload,
+        cuvee: action.payload,
       };
     case cuveeActionTypes.FAILURE:
+      console.log('cuvee failure');
       return {
         ...state,
         hasError: true,
@@ -88,19 +124,21 @@ export const vendorReducer = (state, action) => {
   console.log(` type = ${action.type}`);
   switch (action.type) {
     case vendorActionTypes.REQUEST:
+      console.log('vendor request');
       return {
         ...state,
         isFetching: true,
         hasError: false,
       };
     case vendorActionTypes.SUCCESS:
-      console.log('success');
+      console.log('vendor success');
       return {
         ...state,
         isFetching: false,
         vendors: action.payload,
       };
     case vendorActionTypes.FAILURE:
+      console.log('vendor failure');
       return {
         ...state,
         hasError: true,
